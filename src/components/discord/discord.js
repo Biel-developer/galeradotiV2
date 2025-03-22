8e59004dd36ce5054248158c0dcf1379a24c511a
CaduCarfi <carficadu@gmail.com>
2025-03-22 14:55:44 -0300

    resposividade no discord arrumada

--- src/components/discord/discord.js
+++ src/components/discord/discord.js
@@ -1,15 +1,17 @@
 import "./discord.css";
 import logo from "../../img/Discord-img.jpg";
+
 function Discord() {
-
     return (
-
-        <div>
+        <div className="discord-container">
             <h1>ENTRE NO NOSSO SERVIDOR DO DISCORD !!</h1>
             <div className="discord">
-                <a href="https://discord.gg/KBUjSAeC" target="_blank"><img src={logo} alt="Discord" /></a>
+                <a href="https://discord.gg/KBUjSAeC" target="_blank" rel="noopener noreferrer">
+                    <img src={logo} alt="Discord" />
+                </a>
             </div>
         </div>
-    )
+    );
 }
+
 export default Discord;
